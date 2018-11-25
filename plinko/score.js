9c7e4667cf4cbf13d51e5dabb5a3173df7c0534d
Amruth Pillai <im.amruth@gmail.com>
2018-11-26 01:08:07 +0530

    creating minmax normalization function

--- plinko/score.js
+++ plinko/score.js
@@ -57,3 +57,20 @@ function splitDataset(data, testSetSize) {
 
   return [testSet, trainingSet];
 }
+
+function minmax(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map(row => row[i]);
+
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = ((clonedData[j][i] - min) / (max - min));
+    }
+  }
+
+  return clonedData;
+}
