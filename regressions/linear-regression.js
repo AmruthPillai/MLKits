15a043d8d17372e2234ccd146519411d2ff94680
Amruth Pillai <im.amruth@gmail.com>
2018-11-26 18:48:04 +0530

    initial GD implementation

--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -2,18 +2,29 @@ const tf = require('@tensorflow/tfjs');
 
 class LinearRegression {
 
+  m;
+  b;
+
   features;
   labels;
   options;
 
   constructor(features, labels, options) {
+    this.m = 0;
+    this.b = 0;
+
     this.features = features;
     this.labels = labels;
-
     this.options = Object.assign({
       learningRate: 0.1,
       iterations: 1000
     }, options);
+  }
+
+  gradientDescent() {
+    const currentGuessesForMPG = this.features.map(row => {
+      return this.m * row[0] + this.b;
+    });
   }
 
   train() {
