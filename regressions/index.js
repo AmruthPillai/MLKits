592007c070e0154a34f37bbb5210920a8568cdb9
Amruth Pillai <im.amruth@gmail.com>
2018-11-26 18:39:58 +0530

    creating linear regression class

---
+++ regressions/index.js
@@ -0,0 +1,18 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('./load-csv');
+
+const options = {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ['horsepower'],
+  labelColumns: ['mpg']
+};
+
+let {
+  features,
+  labels,
+  testFeatures,
+  testLabels
+} = loadCSV('./cars.csv', options);
+
